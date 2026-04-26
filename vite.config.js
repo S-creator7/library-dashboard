9a1b0b8286b7b37c5f9fdd07bacce6d0d7374d35
S-creator7 <info@skyrootmarktech.com>
2026-04-27 00:55:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0', // Change this to a valid IP address if needed
+    port: 3003, // Optional otherwise your app will start on default port
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 3003
+  }
 })
